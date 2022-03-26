a84aa05030939f163b05a064b6b1f5c7fa053a60
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2022-03-26 22:55:12 +0100

    feat: Display onboarding screens

--- app/src/main/java/com/dara/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/dara/fitnessapp/MainActivity.kt
@@ -3,41 +3,27 @@ package com.dara.fitnessapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.dara.fitnessapp.ui.theme.FitnessAppTheme
+import com.dara.fitnessapp.ui.theme.OnboardingScreen
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FitnessAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                OnboardingScreen()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FitnessAppTheme {
-        Greeting("Android")
+        OnboardingScreen()
     }
 }
---
+++ app/src/main/java/com/dara/fitnessapp/Onboarding.kt
@@ -0,0 +1,7 @@
+package com.dara.fitnessapp
+
+data class Onboarding(
+    val imageResource: Int,
+    val primaryText: String,
+    val secondaryText: String,
+)
