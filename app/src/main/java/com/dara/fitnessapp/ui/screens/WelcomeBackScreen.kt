74f519ebfa5463c4cd309d1f51676e8bcf82e50f
Olamicode <olalekanf@decagonhq.com>
2022-03-30 21:52:52 +0100

    Added WelcomeBackScreen class

---
+++ app/src/main/java/com/dara/fitnessapp/ui/screens/WelcomeBackScreen.kt
@@ -0,0 +1,14 @@
+package com.dara.fitnessapp.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WelcomeBackScreen() {
+    Column(
+
+    ) {
+
+    }
+}
